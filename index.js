3095c74d41a5acaf25c21c9f57125cde0aad3eb3
Kersten Lutz <kerstenlutz@hotmail.com>
2024-08-20 15:22:23 -0400

    Fixing coeherence of coding structure

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var rs = require('readline-sync');
 
-var operator = 0;
-var userNumOne = 0;
-var userNumTwo = 0;
-var userIndex = [];
-var operatorFunction = {
-    '+' : function(userNumOne, userNumTwo) {
+
+let operatorString = '';                                                                       // Declaring our variable(s), array(s), and object(s)
+let userNumOne = 0;
+let userNumTwo = 0;
+let userNumArray = [];
+let operationsObject = {                                                                       // Listing out all the potential operators that can be chosen and how they interact with the 
+    '+' : function(userNumOne, userNumTwo) {                                                   // user numbers 
         return userNumOne + userNumTwo;
     },
     '-' : function(userNumOne, userNumTwo) {
@@ -20,29 +21,34 @@ var operatorFunction = {
 };
 
 
-function pickOption () {
-    operator = rs.question('Which operator would you like to use? ');
-    if (operator == '*' || operator == '/' || operator == '+' || operator == '-') {
-        console.log('You have chosen ' + operator + '.');
-        userNumOne = rs.questionInt('Which number would you like to use first? ');
-        userIndex.push(userNumOne);
-        userNumTwo = rs.questionInt('Which number would you like to use next? ');
-        userIndex.push(userNumTwo);
+function getOperator () {                                                                       // Set-up an function attaining the operator of choice from the user                  
+    operatorString = rs.question('Which operator would you like to use? ');  
+    if (operationsObject.hasOwnProperty(operatorString)) {                                     // Using the .hasOwnProperty attribute to check if the operator chosen is "Truthy"
+        console.log('You have chosen ' + operatorString + '.');
     } else {
         console.log('That is not a valid operation! Please try again.')
-        pickOption();
+        getOperator();
     }
 }
 
-function userEquation (userIndex, operator) {
-    return userIndex.reduce(operatorFunction[operator]);
+function getNumbers () {     
+    userNumOne = rs.questionInt('Which number would you like to use first? ');
+    userNumArray.push(userNumOne);                                                             // Storing and validating our user's first and second numerical choice
+    userNumTwo = rs.questionInt('Which number would you like to use next? ');
+    userNumArray.push(userNumTwo);
+    console.log('Your equation looks like this, ');                                           // We state the user's built equation
+    console.log(userNumOne + operatorString + userNumTwo);
 }
 
+function userEquationResult () {                                                              // Now we solve the equation with the user inputs
+    console.log('The result of your equation is ');
+    return userNumArray.reduce(operationsObject[operatorString]);                          
+}
 
-pickOption();
+function main () {                                                                            // Function main to control the flow of operation
+    getOperator();
+    getNumbers();
+    console.log(userEquationResult(userNumArray, operatorString));
+}
 
-console.log('Your equation looks like this, ');
-console.log(userNumOne + operator + userNumTwo);
-console.log('The result of your equation is ');
-
-console.log(userEquation(userIndex, operator));
+main ();                                                                                    
